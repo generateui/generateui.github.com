4e0ee7b74e5b6f4eb94ee81e1332189ad28d5e00
rtimon@gmail.com <job_poutsma@hotmail.com>
2012-04-26 13:23:36 +0200

    initial commit

---
+++ DComment.dart
@@ -0,0 +1,7 @@
+class DComment {
+  String text;
+  String uuid;
+  String userUuid;
+  int index;
+  DComment([this.text, this.uuid, this.userUuid]);
+}
---
+++ Drop.dart
@@ -0,0 +1,17 @@
+#import('dart:html');
+#source('Comments.tmpl');
+#source('DComment.dart');
+
+/** A drop for everyone in the cloud */
+class Drop {
+
+  Skeleton() {
+  }
+
+  void run() {
+  }
+}
+
+void main() {
+  new Drop().run();
+}
