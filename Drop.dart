d258670c6f6f0e6640a8e67ac4d1ff933ef7c664
generateui <rtimon@gmail.com>
2012-05-23 15:46:03 +0200

    Make use of hashChanged for Site sections

--- Drop.dart
+++ Drop.dart
@@ -2,18 +2,40 @@
 //#source('Comments.tmpl');
 #source('DComment.dart');
 
-/** A drop for everyone in the cloud */
 class Drop {
-
-  Skeleton() {
+  Drop() {
+    window.on.hashChange.add((e) => loadFromHash());
+    loadFromHash();
   }
 
-  void run() {
-    final req = new XMLHttpRequest.get('article1.html', 
-        (r) => document.query("#content").innerHTML = r.responseText);
+  void loadFromHash() {
+    if (window.location.hash == "") {
+      showPage("welcome");
+    } else {
+      List<String> splitted = window.location.hash.split("/");
+      String section = splitted[0].substring(1);
+      if (splitted.length == 2) {
+        showPage("${section}/${splitted[1]}");
+      } else {
+        showPage("${section}");
+      }
+    }
+  }
+
+  showPage(String relativeUri) {
+    XMLHttpRequest req = new XMLHttpRequest.get("${relativeUri}.html", (oh) {
+      print(oh.status);
+      document.query("#content").innerHTML = oh.responseText;
+    });
+    req.on.error.add((er) => showError()); // does not seem to work :(
+  }
+  showError() {
+    Element error = document.query("#error");
+    error.style.display = "block";
+    document.query("#content").innerHTML = error.innerHTML;
   }
 }
 
 void main() {
-  new Drop().run();
+  new Drop();
 }
